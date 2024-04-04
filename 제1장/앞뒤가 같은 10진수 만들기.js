6f34332b0d868ede41073c926753de0f81958ce3
SamiCoding <samicoding@gmail.com>
2024-04-04 18:04:21 +0900

    앞뒤가 같은 10진수 만들기(JavaScript)

--- 제1장/앞뒤가 같은 10진수 만들기.js
+++ 제1장/앞뒤가 같은 10진수 만들기.js
@@ -1,3 +1,5 @@
+// 10진수, 2진수, 8진수 그 어느 것으로 표현하여도 대칭수가 되는 수 중, 10진수의 10 이상에서의 최솟값을 구해보세요.
+
 // 문자열 형식을 역순으로 반환하는 메서드를 추가
 String.prototype.reverse = function() {
   return this.split("").reverse().join("")
